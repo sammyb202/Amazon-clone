87184342ed49ff1432ccfc5c319d6b6aa573d50e
ordiaprecious <ordiaprecious@gmail.com>
2023-09-29 23:03:50 +0100

    my forth navbar commit

--- main.js
+++ main.js
@@ -1,2 +1,8 @@
+console.log('ggg')
+// )
 
-
+// screen = document.querySelector('screen');
+// screen.onclick = function(){
+//     ham = document.querySelector('.ham')
+//     ham.classlist.toggle('active')
+// }
